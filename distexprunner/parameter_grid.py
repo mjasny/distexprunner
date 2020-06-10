43c0a0abe30738b523b3d2ce061e16413d9a600a
mjasny <matthiaschristian.jasny@stud.tu-darmstadt.de>
2020-06-10 09:21:15 +0200

    remove old code comment

--- distexprunner/parameter_grid.py
+++ distexprunner/parameter_grid.py
@@ -1,19 +1,5 @@
-
-
-# class Grid:
-#     def __init__(self, factory_fn, *args):
-#         for params in itertools.product(*args):
-#             cls = factory_fn(*params)
-
-#             if not issubclass(cls, experiment.Base):
-#                 raise Exception('Factory needs to return a child of experiment.Base')
-
-#             suffix = '_'.join(map(str, params))
-#             cls.__name__ += f'_{suffix}'
-#             logging.info(f'Generated experiment: {cls.__name__}')
-#             generated_experiments.append(cls)
-
 import itertools
+
 
 class ParameterGrid:
     def __init__(self, **kwargs):
