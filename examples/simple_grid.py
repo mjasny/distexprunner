f604c65c5d22789b84aca2434af24c9f460d3612
mjasny <matthiaschristian.jasny@stud.tu-darmstadt.de>
2020-11-18 14:14:02 +0100

    add computed param example

--- examples/simple_grid.py
+++ examples/simple_grid.py
@@ -7,17 +7,19 @@ server_list = config.server_list[0, ]
 parameter_grid = ParameterGrid(
     a=range(1, 5),
     b=[2, 4],
-    to_file=[True, False]
+    to_file=[True, False],
+    computed=ComputedParam(lambda to_file: [1] if to_file else 2)
 )
 
+
 @reg_exp(servers=server_list, params=parameter_grid)
-def simple_grid(servers, a, b, to_file):
+def simple_grid(servers, a, b, to_file, computed):
     for s in servers:
         stdout = File('simple_grid.log', append=True)
         if not to_file:
             stdout = [stdout, Console(fmt=f'{s.id}: %s')]
 
-        s.run_cmd(f'echo {a} {b}', stdout=stdout).wait()
+        s.run_cmd(f'echo {a} {b} {computed}', stdout=stdout).wait()
 
 
 @reg_exp(servers=ServerList())
