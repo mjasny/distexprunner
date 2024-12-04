85eac73386aab912b0a7f44094c2c27996062c62
mjasny <matthias.jasny@cs.tu-darmstadt.de>
2024-12-04 11:51:27 +0100

    add shell-nix

---
+++ shell.nix
@@ -0,0 +1,16 @@
+let
+  pkgs = import <nixpkgs> {};
+  distexprunner = pkgs.python3Packages.buildPythonPackage {
+    name = "distexprunner";
+    src = ./.;
+    format = "pyproject";
+    propagatedBuildInputs = with pkgs.python3Packages; [ setuptools ];
+  };
+in
+pkgs.mkShell {
+  packages = [
+    (pkgs.python3.withPackages (python-pkgs: [
+      distexprunner
+    ]))
+  ];
+}
